ce5de0e22c07a73106f37cb1fd6dbe1939315390
ding <69354087+prgding@users.noreply.github.com>
2023-03-11 23:25:59 +0800

    result 动画修复

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 function checkTriangle() {
-	let side1 = parseInt(document.getElementById("side1").value);
-	let side2 = parseInt(document.getElementById("side2").value);
-	let side3 = parseInt(document.getElementById("side3").value);
+	cloneTag();
+	let side1 = parseFloat(document.getElementById("side1").value);
+	let side2 = parseFloat(document.getElementById("side2").value);
+	let side3 = parseFloat(document.getElementById("side3").value);
+	
 
 	if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
 		if (side1 === side2 && side1 === side3) {
@@ -19,9 +21,11 @@ function checkTriangle() {
 }
 
 function showRules() {
+	cloneTag();
 	document.getElementById("result").innerText = "三角形问题规则：\n\n" +
 		"1. a, b, c 均大于0\n" +
 		"2. a + b > c, b + c > a, c + a > b";
+	
 }
 
 function Clear() {
@@ -30,3 +34,28 @@ function Clear() {
 	document.getElementById("side3").value = ''
 	document.getElementById("result").innerText = ''
 }
+
+function clearResult() {
+	document.getElementById("result").innerText = ''
+}
+
+function cloneTag(){
+	let resultElt = document.getElementById("result");
+	let newElement = resultElt.cloneNode(true);
+	resultElt.parentNode.replaceChild(newElement, resultElt);
+}
+
+function handleEnterKey(event) {
+	if (event.key === 'Enter') {
+		checkTriangle();
+	}
+}
+
+onload = function (){
+	document.getElementById("side1").addEventListener("keydown", handleEnterKey);
+	document.getElementById("side2").addEventListener("keydown", handleEnterKey);
+	document.getElementById("side3").addEventListener("keydown", handleEnterKey);
+}
+
+
+
